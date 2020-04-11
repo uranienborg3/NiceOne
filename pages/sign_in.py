2ddc654502ac5d5ad4c29fcf9b40755f9120dd4f
uranienborg3 <uranienborg3@gmail.com>
2020-04-11 17:21:52 +0300

    added register tests; added sign in test

--- pages/sign_in.py
+++ pages/sign_in.py
@@ -1,8 +1,13 @@
+import time
+import random
 from .base import BasePage
 from .base import InvalidPageException
 from .locators import SignInLocators
 from .locators import BreadcrumbsLocators
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.select import Select
 
 
 class SignIn(BasePage):
@@ -23,3 +28,67 @@ class SignIn(BasePage):
     def authentication_should_be_in_breadcrumbs(self):
         assert self.is_element_present(*BreadcrumbsLocators.AUTHENTICATION_BREADCRUMB),\
             "Authentication not in breadcrumb"
+
+    def enter_email(self, email):
+        field = self._find(*SignInLocators.EMAIL_FIELD)
+        field.send_keys(email)
+        field.submit()
+
+    def fill_in_name_and_password(self, name, surname, passwd):
+        gender_checkboxes = WebDriverWait(self.browser, 5).until(ec.presence_of_all_elements_located
+                                                                 ((SignInLocators.GENDER_CHECKBOXES)))
+        random.choice(gender_checkboxes).click()
+        first_name_field = self._find(*SignInLocators.FIRST_NAME_FIELD)
+        first_name_field.send_keys(name)
+        second_name_field = self._find(*SignInLocators.SECOND_NAME_FIELD)
+        second_name_field.send_keys(surname)
+        password = self._find(*SignInLocators.PASSWORD_FIELD)
+        password.send_keys(passwd)
+
+    def set_birthday(self):
+        self._get_random_option(*SignInLocators.BIRTH_YEAR)
+        self._get_random_option(*SignInLocators.BIRTH_MONTH)
+        self._get_random_option(*SignInLocators.BIRTH_DAY)
+
+    def fill_in_address_and_country(self, company, add_1, add_2, city, postcode):
+        company_f = self._find(*SignInLocators.COMPANY_FIELD)
+        company_f.send_keys(company)
+        address_1 = self._find(*SignInLocators.FIRST_ADDRESS_FIELD)
+        address_1.send_keys(add_1)
+        address_2 = self._find(*SignInLocators.SECOND_ADDRESS_FIELD)
+        address_2.send_keys(add_2)
+        city_f = self._find(*SignInLocators.CITY_FIELD)
+        city_f.send_keys(city)
+        self._get_random_option(*SignInLocators.STATE_LIST)
+        postcode_f = self._find(*SignInLocators.POSTCODE_FIELD)
+        postcode_f.send_keys(postcode)
+
+    def fill_in_phone_and_add_info(self, add_info, home_phone, mobile):
+        add_info_f = self._find(*SignInLocators.ADDITIONAL_INFO_FIELD)
+        add_info_f.send_keys(add_info)
+        home_phone_f = self._find(*SignInLocators.HOME_PHONE_FIELD)
+        home_phone_f.send_keys(home_phone)
+        mobile_f = self._find(*SignInLocators.MOBILE_FIELD)
+        mobile_f.send_keys(mobile)
+        time.sleep(4)
+
+    def register_account(self):
+        button = self._find(*SignInLocators.SUBMIT_BUTTON)
+        button.click()
+
+    def sign_in(self, email, password):
+        email_f = self._find(*SignInLocators.SIGN_IN_EMAIL_FIELD)
+        email_f.send_keys(email)
+        passwd_f = self._find(*SignInLocators.SIGN_IN_PASSWORD_FIELD)
+        passwd_f.send_keys(password)
+        button = self._find(*SignInLocators.SIGN_IN_BUTTON)
+        button.click()
+
+    def _get_random_option(self, how, what):
+        select_element = Select(self.browser.find_element(how, what))
+        options = select_element.options
+        select_element.select_by_index(random.randint(1, len(options)-1))
+
+    def _find(self, how, what):
+        element = self.browser.find_element(how, what)
+        return element
--- sign_in_test.py
+++ sign_in_test.py
@@ -1,11 +1,63 @@
+import pytest
+import random
 from pages.home_page import HomePage
 from pages.sign_in import SignIn
+from faker import Faker
 
 
+@pytest.mark.sign_in
 class TestSignIn:
-    def test_guest_can_sign_in(self, browser):
+    @pytest.fixture(scope="function")
+    def register(self):
+        fake = Faker()
+        self.data = {"email": str(random.randint(1, 100)) + fake.email(),
+                     "name": fake.first_name(),
+                     "surname": fake.last_name(),
+                     "passwd": fake.password(length=5, special_chars=False),
+                     "address_1": fake.street_address(),
+                     "address_2": fake.secondary_address(),
+                     "city": fake.city(),
+                     "postcode": fake.postcode(),
+                     "company": fake.company(),
+                     "mobile": "+1" + str(random.random())[-10:],
+                     "home_phone": "+1" + str(random.random())[-10:],
+                     "add_info": fake.sentence(nb_words=10)}
+        test_data = open("test_data.txt", "a")
+        test_data.write("Credentials:\n")
+        for key, value in self.data.items():
+            test_data.write(f"{key}: {value}\n")
+        test_data.close()
+
+    def test_guest_can_go_to_sign_in(self, browser):
         home_page = HomePage(browser)
         home_page.go_to_sign_in()
         sign_in_page = SignIn(browser)
         sign_in_page.should_be_authentication_header()
         sign_in_page.authentication_should_be_in_breadcrumbs()
+
+    @pytest.mark.register
+    @pytest.mark.skip
+    def test_guest_can_register(self, browser, register):
+        home_page = HomePage(browser)
+        home_page.go_to_sign_in()
+        sign_in_page = SignIn(browser)
+        sign_in_page.enter_email(self.data.get("email"))
+        sign_in_page.fill_in_name_and_password(self.data.get("name"),
+                                               self.data.get("surname"),
+                                               self.data.get("passwd"))
+        sign_in_page.set_birthday()
+        sign_in_page.fill_in_address_and_country(self.data.get("company"),
+                                                 self.data.get("address_1"),
+                                                 self.data.get("address_2"),
+                                                 self.data.get("city"),
+                                                 self.data.get("postcode"))
+        sign_in_page.fill_in_phone_and_add_info(self.data.get("add_info"),
+                                                self.data.get("home_phone"),
+                                                self.data.get("mobile"))
+        sign_in_page.register_account()
+
+    def test_user_can_sign_in(self, browser):
+        home_page = HomePage(browser)
+        home_page.go_to_sign_in()
+        sign_in_page = SignIn(browser)
+        sign_in_page.sign_in('43bennettstephanie@yahoo.com', 'vh6Va')
